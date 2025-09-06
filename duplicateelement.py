ddc632a4f74ff075144409cd8fed7403d352ed21
Tirth <tirtheshkarande@gmail.com>
2025-09-06 18:02:54 +0530

    other scenarios added to check element comparey

--- duplicateelement.py
+++ duplicateelement.py
@@ -1,19 +1,33 @@
 # in arrary any duplicate element is present then return True otherwise false
 from utils import TestResult
 
-
 def find_duplicate1(arr):
     # if the lenth of array is equal to the length od set created from array then return false otherwise true
     return len(arr) != len(set(arr))
 
 def find_duplicate2(arr):
-    unique_elements=set()
+    # create set which contain the unique element ,compare each element from arr  to  unique_elements if present the return true else add element in unique_elements.
+    # return the final result as false after loop complete.
+    unique_elements = set()
     for i in arr:
         if i in unique_elements:
             return True
         else:
             unique_elements.add(i)
     return False
+
+
+def find_duplicate3(arr):
+    # using brute force
+    # compare each element from arr and check element is present or not
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            # print('i and j',i,j)
+            if arr[i] == arr[j]:
+                return True
+    return False
+
 
 if __name__ == "__main__":
     result_check_data = [{'param': [1, 2, 3, 3], 'result': True}, {
@@ -22,3 +36,4 @@ if __name__ == "__main__":
     testobj = TestResult(result_check_data)
     testobj.test_all_scenarios(find_duplicate1)
     testobj.test_all_scenarios(find_duplicate2)
+    testobj.test_all_scenarios(find_duplicate3)
