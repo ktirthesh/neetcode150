4bfde106a6589eb76fa4909df783395a08ae9131
Tirth <tirtheshkarande@gmail.com>
2025-09-07 06:16:44 +0530

    remname files and updated utils

--- 1.duplicateelement.py
+++ 1.duplicateelement.py
@@ -1,11 +1,13 @@
 # in arrary any duplicate element is present then return True otherwise false
 from utils import TestResult
 
-def find_duplicate1(arr):
+
+def find_duplicate1(arr: list) -> bool:
     # if the lenth of array is equal to the length od set created from array then return false otherwise true
     return len(arr) != len(set(arr))
 
-def find_duplicate2(arr):
+
+def find_duplicate2(arr: list) -> bool:
     # create set which contain the unique element ,compare each element from arr  to  unique_elements if present the return true else add element in unique_elements.
     # return the final result as false after loop complete.
     unique_elements = set()
@@ -17,7 +19,7 @@ def find_duplicate2(arr):
     return False
 
 
-def find_duplicate3(arr):
+def find_duplicate3(arr: list) -> bool:
     # using brute force
     # compare each element from arr and check element is present or not
     n = len(arr)
@@ -29,11 +31,21 @@ def find_duplicate3(arr):
     return False
 
 
+def find_duplicate4(arr: list):
+    # sort the arr, check adjecnt element is equal or not
+    arr1 = arr.sort()
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i-1]:
+            return True
+    return False
+
+
 if __name__ == "__main__":
-    result_check_data = [{'param': [1, 2, 3, 3], 'result': True}, {
-        'param': [1, 2, 3, 4], 'result': False}]
+    result_check_data = [{'params': {'arr':[1, 2, 3, 3]}, 'result': True}, {
+        'params': {'arr':[1, 2, 3, 4]}, 'result': False}]
 
     testobj = TestResult(result_check_data)
     testobj.test_all_scenarios(find_duplicate1)
     testobj.test_all_scenarios(find_duplicate2)
     testobj.test_all_scenarios(find_duplicate3)
+    testobj.test_all_scenarios(find_duplicate4)
--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
 class TestResult:
-    color = {True :"\033[92m",False:"\033[91m"}
-    
-    def __init__(self,scenarios,):
+    color = {
+        True: "\033[92m",  # green
+        False: "\033[91m"  # red
+    }
+
+    def __init__(self, scenarios,):
         self.scenarios = scenarios
         self.executer = None
-        
 
-    def find_result(self,result ,params):
-        print(f"{self.color[result  == self.executer(params)]}|Function name : {self.executer.__name__} | parameters = {params} | Expected result : {result} | Actual result : {self.executer(params)}\n \033[0m")
-        
-    def test_all_scenarios(self,executer):
+    def show_result(self, result, params):
+        print(f"{self.color[result  == self.executer(**params)]}Function name : {self.executer.__name__} | parameters = {params} | Expected result : {result} | Actual result : {self.executer(**params)}\n \033[0m")
+
+    def test_all_scenarios(self, executer):
         self.executer = executer
         for ele in self.scenarios:
-            self.find_result(ele['result'],ele['param'],)
-
+            self.show_result(ele['result'], ele['params'],)
